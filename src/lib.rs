f41dcd61464b96b0ff233dc2feab0528d044d909
Andrew Rowles <andrew@rowles.io>
2019-08-17 13:34:33 -0400

    simple search impl

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,15 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 
@@ -46,7 +54,7 @@ safe, fast, productive.
 Pick three.";
 
         assert_eq!(
-            vec!["safe, fast, productive"],
+            vec!["safe, fast, productive."],
             search(query, contents)
         );
     }
