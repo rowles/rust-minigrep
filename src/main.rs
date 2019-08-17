3f2e4c257fb8480f6e0a7d67682f1023e9b4af45
Andrew Rowles <andrew@rowles.io>
2019-08-17 13:13:33 -0400

    config parse struct

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,26 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
-    //let query = &args[1];
-    //let filename = &args[2];
+    let config = parse_config(&args);
 
-    println!("Search for {}", query);
-    println!("In file {}", filename);
+    println!("Search for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+struct Config {
+    query: String,
+    filename: String,
+}
 
-    (query, filename)
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
 
